41812e3cfcd646631ac57e296e1aaba3e0f538c0
Yusuke Asai <ptx014@gmail.com>
2024-08-04 18:36:18 +0900

    Github Pagesで相対パスを参照するように設定変更

--- chorin/vite.config.ts
+++ chorin/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: process.env.GITHUB_PAGES  // ‚±‚Ìs‚ğ’Ç‰Á
+        ? "reactailwind"            // ‚±‚Ìs‚ğ’Ç‰Á
+        : "./",                     // ‚±‚Ìs‚ğ’Ç‰Á
   plugins: [react()],
 })
