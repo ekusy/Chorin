3d76c0eb9530e9981f35ecc551d3ef0f4a533806
Yusuke Asai <ptx014@gmail.com>
2024-08-07 21:34:41 +0900

    支払い一覧テーブルのコンポーネント作成

---
+++ chorin/src/componets/table/PaymentsTable.tsx
@@ -0,0 +1,50 @@
+﻿import { useState } from 'react'
+import './PaymentsTable.css'
+
+interface PaymentsTableProps {
+    id: string;
+}
+
+interface Payment {
+    expense: number;    // 金額
+    purpose: string;    // 目的
+    payer: string;      // 払った人
+}
+
+function PaymentsTable({ id }: PaymentsTableProps) {
+
+    const [payments, setPaymants] = useState(Array<Payment>());
+
+    function UpdatePayments() { 
+        setPaymants([...payments, { expense: getRandomInt(1000), purpose: "太郎", payer: "なにか"}])
+    }
+    function getRandomInt(max:number) {
+        return Math.floor(Math.random() * max);
+    }
+
+    return (
+        <div>
+            <h1>対象:{id}</h1>
+            <button onClick={UpdatePayments}>行追加</button>
+            <table>
+                <tr>
+                    <th>支払者</th>
+                    <th>目的</th>
+                    <th>金額</th>
+                </tr>
+                {payments.map((payment) => (
+                    <tr>
+                        <td>{payment.payer}</td>
+                        <td>{payment.purpose}</td>
+                        <td>{payment.expense}</td>
+                    </tr>
+                ))};
+
+            </table>
+        </div>
+    );
+}
+
+export default PaymentsTable;
+
+           
